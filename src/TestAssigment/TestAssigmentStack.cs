98986511a4fa58cd6c6a4b8de723b077b3c0c511
Arthur Markus <arthurmarkus2013@outlook.com>
2022-08-29 17:40:54 +0200

    Initial commit

---
+++ src/TestAssigment/TestAssigmentStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace TestAssigment
+{
+    public class TestAssigmentStack : Stack
+    {
+        internal TestAssigmentStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
